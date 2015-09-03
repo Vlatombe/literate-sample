fd83b4f3734744e620c6c1d5f5c4266a7ac3be54
Vincent Latombe <vincent@latombe.net>
2015-09-03 10:58:16 -0700

    Create a maven project

---
+++ literate-sample/src/main/java/org/juc/us/west/App.java
@@ -0,0 +1,13 @@
+package org.juc.us.west;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
